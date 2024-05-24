856398e21a1cc3875c48b88e4258b3cda0c337f7
Tienna2123 <kaidanchoi@gmail.com>
2024-05-24 15:46:36 +0700

    lab2

---
+++ Bai2_Lab2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2_Lab2
+{
+    internal class Program
+    {
+        public void bai2a()
+        {
+            var userInfo = new
+            {
+                id = "2024",
+                name = "faker",
+                isplaying = false,
+                bag = new
+                {
+                    skins = 0,
+                    cups = 0
+                }
+            };
+            Debug.WriteLine("id: " + userInfo.id);
+            Debug.WriteLine("name: " + userInfo.name);
+            Debug.WriteLine("isplaying: " + userInfo.isplaying);
+            Debug.WriteLine("bag skins: " + userInfo.bag.skins);
+            Debug.WriteLine("bag cups: " + userInfo.bag.cups);
+        }
+
+        public void bai2b()
+        {
+            int y = 10;
+            Action<int> AnonymousMethod = delegate (int x)
+            {
+                int sum = x + y;
+                int sub = x - y;
+                Debug.WriteLine("Sum: " + sum);
+                Debug.WriteLine("Sub: " + sub);
+            };
+            AnonymousMethod(5);
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+        
+       
+    
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+public class UserData
+{
+    public int id;
+    public string userName;
+    public int level;
+    public UserData(int id, string userName, int level)
+    {
+        this.id = id;
+        this.userName = userName;
+        this.level = level;
+    }
+    public void ShowData()
+    {
+        Console.WriteLine("ID: " + id);
+        Console.WriteLine("User Name: " + userName);
+        Console.WriteLine("Level: " + level);
+    }
+    public void ShowData(string name, dynamic attribute)
+    {
+        Console.WriteLine(name + ": " + attribute);
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Bai 1a");
+        var x = 10;
+        void bai1a()
+        {
+            Console.WriteLine("x = " + x);
+        }
+        bai1a();
+        Console.WriteLine();
+
+        Console.WriteLine("Bai 1b");
+        void getDetail(dynamic value)
+        {
+            Console.WriteLine("Value: " + value);
+        }
+        void bai1b()
+        {
+            getDetail(10);
+            getDetail("Game");
+
+        }
+        bai1b();
+        Console.WriteLine();
+
+
+        Console.WriteLine("Bai 1c");
+        UserData userData = new UserData(0, "first player", 10);
+        userData.ShowData("level", userData.level);
+        userData.ShowData("userName", userData.userName);
+        userData.ShowData("id", userData.id);
+        Console.WriteLine();
+
+
+    }
+
+}
